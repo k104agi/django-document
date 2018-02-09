3c251bf6bbf4b4944c51d8ba76afbb7be94d8edc
kjs <k104agi@gmail.com>
2018-02-09 12:25:43 +0900

    oo

--- django/inheritance/multi_table/models.py
+++ django/inheritance/multi_table/models.py
@@ -4,6 +4,12 @@ from django.db import models
 class Place(models.Model):
     name = models.CharField(max_length=50)
     address = models.CharField(max_length=80)
+    # other = models.ForeignKey(
+    #     Other, on_delete=models.SET_NULL,
+    #     related_name='places',
+    #     related_query_name='restaurants',
+    #     blank=True, null=True,
+    # )
 
     def __str__(self):
         return f'{self.name} | {self.address}'
@@ -12,6 +18,10 @@ class Place(models.Model):
 class Restaurant(Place):
     serves_hot_dogs = models.BooleanField(default=False)
     serves_pizza = models.BooleanField(default=False)
+    nearby_places = models.ManyToManyField(
+        Place,
+        related_query_name='near_restaurant'
+    )
 
     def __str__(self):
         return f'Restaurant {self.name} | {self.address}'
