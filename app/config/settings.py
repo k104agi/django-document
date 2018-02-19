db29661f7ba578f79fb39e3cc067187176b7b779
kjs <k104agi@gmail.com>
2018-02-19 12:53:43 +0900

    popostgresql로 DB 변경함

--- app/config/settings.py
+++ app/config/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fc-django-document',
+        'USER': 'kjs',
+        'PASSWORD':'gozldshsh1!',
+        'HOST':'localhost',
+        'PORT':'5432',
     }
 }
 
