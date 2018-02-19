4588f06f764c0c26a10698174f689c3b950a4fe0
kjs <k104agi@gmail.com>
2018-02-19 11:53:45 +0900

    weblog 만든 후 마이그레이션 하고커밋

--- app/config/settings.py
+++ app/config/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
     'inheritance.proxy_manager.apps.ProxyManagerConfig',
     'inheritance.multiple_inheritance.apps.MultipleInheritanceConfig',
 
+    'weblog.apps.WeblogConfig',
 ]
 
 MIDDLEWARE = [
